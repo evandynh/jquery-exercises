9b1e09a5509783f79f0eb444b0f86be5780f5572
Eric Van Dyn Hoven <eric.vandynhoven@gmail.com>
2017-02-01 11:55:54 -0800

    Inital commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$( document ).ready(function{
+
+  console.log("connected!");
+
+  $('#container').click(function(){
+    alert('You clicked the container div')
+  })
+
+  $('#myBtn').click(function(){
+    alert('You clicked me!')
+    event.stopPropagation()
+  })
+
+
+  $('#foods').append("<li>Mac n Cheese</li><li>Pizza</li><li>Cheeseburgers</li>")
+
+  $('#drinks').append("<li>Apple Juice</li><li>Tea</li><li>Water</li>")
+
+  $('#addFood').click(function(){
+    var newFood = $('#foodInput').val()
+    if(newFood != ""){
+      $('#foods').append("<li>" + newFood + "</li>")
+      $('#foodInput').val('')
+    }
+  })
+
+  $('#addDrink').click(function(){
+    var newDrink = $('#drinkInput').val()
+    if(newDrink != ""){
+      $('#drinks').append("<li>" + newDrink + "</li>")
+      $('#drinkInput').val('')
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+})
